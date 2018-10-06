1267310314bee32ec5b65fb543732a4dd44b535d
humanoiddess13 <zarinabakhytzhan@gmail.com>
2018-10-07 02:38:38 +0600

    wrote function for get_min_with_one_or_more_args(first,*args)

--- find_min.py
+++ find_min.py
@@ -37,6 +37,12 @@ def get_min_with_one_or_more_arguments(first, *args):
     """
         return smallest number among first + args
     """
+    result=first
+    for i in args:
+        if i < result:
+            result = i
+
+    return result
 
 
 def get_min_bounded(*args, low, high):
